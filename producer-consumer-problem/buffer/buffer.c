e24790fecf42708ef6b308466f59527896693e84
Ryan Toh <ryedralisk@gmail.com>
2022-07-06 16:00:11 +0800

    refactor: minimum lock usage

--- producer-consumer-problem/buffer/buffer.c
+++ producer-consumer-problem/buffer/buffer.c
@@ -4,16 +4,13 @@ buffer_item buffer[BUFFER_SIZE];
 
 int read_index = 0;
 int write_index = 0;
-sem_t  read_sem;
-sem_t write_sem;
-pthread_mutex_t read_mutex;
-pthread_mutex_t write_mutex;
+sem_t  read_sem; // represents filled spots that can be consumed
+sem_t write_sem; // represents empty spaces that can be written to
+pthread_mutex_t access_mutex;
 
 void buffer_init()
 {
-    pthread_mutex_init(&read_mutex,  NULL);
-    pthread_mutex_init(&write_mutex, NULL);
-    //sem_post(&write_sem); // start writing
+    pthread_mutex_init(&access_mutex, NULL);
     for (int i = 0; i < BUFFER_SIZE; i++) {
         buffer[i] = -1;
     }
@@ -29,22 +26,22 @@ void buffer_init()
  */
 int insert_item(buffer_item item)
 {
-    pthread_mutex_lock(&write_mutex); // make sure no other producer threads can write
     sem_wait(&write_sem); // wait until can write
+    pthread_mutex_lock(&access_mutex); // make sure no other threads can write
     // CRITICAL SECTION START
     print_buffer_state();
     buffer_item writeVal = buffer[write_index];
     if (writeVal && writeVal != -1) { // writing to valid value
         // CRITICAL SECTION END
         sem_post(&read_sem);
-        pthread_mutex_unlock(&write_mutex); // release lock on this CS
+        pthread_mutex_unlock(&access_mutex); // release lock on this CS
         return -1;
     }
     buffer[write_index] = item;
     write_index = (write_index + 1) % BUFFER_SIZE;
     // CRITICAL SECTION END
+    pthread_mutex_unlock(&access_mutex); // release lock on this CS
     sem_post(&read_sem);  // notify can read
-    pthread_mutex_unlock(&write_mutex); // release lock on this CS
     return 0;
 }
 
@@ -56,23 +53,23 @@ int insert_item(buffer_item item)
  */
 int remove_item(buffer_item *item)
 {
-    pthread_mutex_lock(&read_mutex); // make sure no other consumer threads can write
     sem_wait(&read_sem); // wait until can read
+    pthread_mutex_lock(&access_mutex); // make sure no other threads can read
     // CRITICAL SECTION START
     print_buffer_state();
     buffer_item readVal = buffer[read_index];
     if (!readVal || readVal == -1) { // tried to read invalid value
         // CRITICAL SECTION END
         sem_post(&write_sem); // free write_sem
-        pthread_mutex_unlock(&read_mutex); // release lock on this CS
+        pthread_mutex_unlock(&access_mutex); // release lock on this CS
         return -1;
     }
     *item = readVal;
     buffer[read_index] = -1;
     read_index = (read_index + 1) % BUFFER_SIZE;
     // CRITICAL SECTION END
+    pthread_mutex_unlock(&access_mutex); // release lock on this CS
     sem_post(&write_sem);
-    pthread_mutex_unlock(&read_mutex); // release lock on this CS
     return 0;
 }
 

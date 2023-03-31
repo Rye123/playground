756f0c175d7310723b1f9f083fdd867a3ff12b18
Ryan Toh <ryedralisk@gmail.com>
2023-03-31 19:06:47 +0800

    feat: reduce

--- src/pyfunc.py
+++ src/pyfunc.py
@@ -9,3 +9,20 @@ def map(fn: Callable, ls: List[Any], *args, **kwargs) -> List[Any]:
     for i in range(len(ret)):
         ret[i] = fn(ret[i], *args, **kwargs)
     return ret
+
+def reduce(fn: Callable[[Any, Any], Any], ls: List[Any], initialiser=None) -> Any:
+    """
+    Aggregates values by applying an aggregation function `fn` \
+    sequentially on `ls` and returning the final result.
+    - `fn` is a function that takes two arguments:
+        - The left argument is the accumulated value.
+        - The right argument is the next value in the list.
+    """
+    if len(ls) == 0:
+        raise ValueError("Cannot reduce an empty list.")
+    if len(ls) == 1:
+        if initialiser is not None:
+            return fn(initialiser, ls[0])
+        return ls[0]
+    acc = reduce(fn, ls[:-1], initialiser) # Accumulated value
+    return fn(acc, ls[-1])
--- tests/pyfunc_test.py
+++ tests/pyfunc_test.py
@@ -29,4 +29,28 @@ class MapTest(unittest.TestCase):
         ls2_test = pyfunc.map(add_elem, ls2, 2)
         self.assertEqual(ls2_test, ls2_exp)
 
+class ReduceTest(unittest.TestCase):
+    def test_reduce_addition(self):
+        ls1 = [1, 2, 3, 4, 5, 6]
+        ls1_exp_1 = sum(ls1)
+        ls1_exp_2 = sum(ls1) + 6
 
+        ls1_test_1 = pyfunc.reduce(lambda x, y: x + y, ls1)
+        ls1_test_2 = pyfunc.reduce(lambda x, y: x + y, ls1, 6)
+        self.assertEqual(ls1_test_1, ls1_exp_1)
+        self.assertEqual(ls1_test_2, ls1_exp_2)
+
+    def test_reduce_lists(self):
+        ls1 = [[1, 2], [3, 4], [5, 6]]
+        ls1_exp_1 = [1, 2, 3, 4, 5, 6]
+
+        ls1_test_1 = pyfunc.reduce(lambda x, y: x + y, ls1) 
+        self.assertEqual(ls1_test_1, ls1_exp_1)
+
+    def test_reduce_strings(self):
+        ls1 = ["cat", "dog", "elephant", "fish"]
+        def join_str(x, y):
+            return f"{x} {y}"
+        ls1_exp = "cat dog elephant fish"
+        ls1_test = pyfunc.reduce(join_str, ls1)
+        self.assertEqual(ls1_test, ls1_exp)

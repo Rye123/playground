c7e7f3e89af6be7b08da4e6389409acf485811e8
Ryan Toh <ryantoh99@gmail.com>
2023-05-28 23:55:47 +0800

    refactor: frame reading

--- accelerated-cpp/character_framing/main.cpp
+++ accelerated-cpp/character_framing/main.cpp
@@ -7,16 +7,43 @@
 
 using std::string; using std::vector;
 using std::cin; using std::cout; using std::endl;
+using std::istream;
+
+/**
+ * Read lines from a given `istream`.
+*/
+istream& readLines(istream& is, vector<string>& lines)
+{
+	// Ensure no failure state
+	if (!is)
+		return is;
+
+	string s;
+	while (getline(is, s))
+		lines.push_back(s);
+	is.clear();
+	return is;
+}
+
+istream& readFrame(istream& is, vector<string>& frame)
+{
+	// Ensure no failure state
+	if (!is)
+		return is;
+
+	vector<string> lines;
+	readLines(is, lines);
+	frame = getFrame(lines);
+	is.clear();
+	return is;
+}
 
 int main()
 {
-	vector<string> lines;	
-	string s;
-	while (getline(cin, s)) {
-		lines.push_back(s);	
-	}
-	vector<string> framed = getFrame(lines);
-	s = getFrameString(framed);
+	vector<string> framed;
+	readFrame(cin, framed);
+	string s = getFrameString(framed);
 	cout << s << endl;
+
 	return 0;
 }

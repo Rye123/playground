08bf41cf92947f672af565333df9e09e3281eabe
Ryan Toh <ryantoh99@gmail.com>
2023-05-30 17:29:06 +0800

    feat: detect.cpp

--- accelerated-cpp/ascender-descender-detection/detect.cpp
+++ accelerated-cpp/ascender-descender-detection/detect.cpp
@@ -1,16 +1,36 @@
 #include "detect.h"
 #include <list>
 #include <string>
+#include <cctype>
 
 using std::list; using std::string;
 
+bool isInList(const char& c, list<char> ls)
+{
+	for (char lsc : ls) {
+		if (lsc == c)
+			return true;
+	}
+	return false;
+}
+
 bool containsAscenders(const string& s)
 {
+	for (char c : s) {
+		c = tolower(c);
+		if (isInList(c, ASCENDERS))
+			return true;
+	}
 	return false;
 }
 
 bool containsDescenders(const string& s)
 {
+	for (char c : s) {
+		c = tolower(c);
+		if (isInList(c, DESCENDERS))
+			return true;
+	}
 	return false;
 }
 

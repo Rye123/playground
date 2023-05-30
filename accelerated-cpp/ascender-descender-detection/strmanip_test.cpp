b0fa515629b5d46e69b64fd57a56c646ec963831
Ryan Toh <ryantoh99@gmail.com>
2023-05-30 17:45:57 +0800

    refactor: remove dumb not equals

--- accelerated-cpp/ascender-descender-detection/strmanip_test.cpp
+++ accelerated-cpp/ascender-descender-detection/strmanip_test.cpp
@@ -12,25 +12,26 @@ bool test_split()
 {
 	string s0 = "The quick brown fox";
 	vector<string> w0{ "The", "quick", "brown", "fox" };
-	if (!(split(s0) == w0))
+	if (split(s0) != w0)
 		return false;
 
 	string s1 = "The\nquick\nbrown\nfox";
-	if (!(split(s1) == w0))
+	if (split(s1) != w0)
 		return false;
 
 	string s2 = "The";
 	vector<string> w2{ "The" };
-	if (!(split(s2) == w2))
+	if (split(s2) != w2)
 		return false;
 
 	string s3 = "   The   \n";
-	if (!(split(s3) == w2))
+	if (split(s3) != w2)
 		return false;
 
 	string s4 = "\tThe\tquick brown\n fox";
-	if (!(split(s4) == w0))
+	if (split(s4) != w0)
 		return false;
+
 	return true;
 }
 

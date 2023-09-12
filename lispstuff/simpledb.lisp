969eff38ff40b10cb7ba3d3f3b60b03c4edcc458
Ryan Toh <ryedralisk@gmail.com>
2023-09-12 13:20:22 +0800

    simpledb.lisp: feat: add db methods and sorting predicates

--- lispstuff/simpledb.lisp
+++ lispstuff/simpledb.lisp
@@ -22,6 +22,36 @@
   " Takes a name string and a date list (year, month, day), and stores it as a name and timestamp. "
   (push (list :name name :dob (apply 'encode-date date)) *db*))
 
-(defun dump-db ()
-  (dolist (record *db*)
+(defun record-by-dob-p (r1 r2)
+  (< (getf r1 :dob) (getf r2 :dob)))
+
+(defun record-by-birthday-p (r1 r2)
+  " Predicate that sorts by birthday (i.e. month and day) "
+  (let ((d1 (decode-date (getf r1 :dob)))
+	(d2 (decode-date (getf r2 :dob))))
+    (if (equal (getf d1 :month) (getf d2 :month))
+	(< (getf d1 :day) (getf d2 :day))
+     (< (getf d1 :month) (getf d2 :month)))))
+
+(defun dump-db (database)
+  (dolist (record database)
     (format t "~a: ~10t~a~%" (getf record :name) (stringify-date (getf record :dob)))))
+
+(defun dump-db-sort-by-dob (database)
+  (dump-db (sort database 'record-by-dob-p)))
+
+(defun dump-db-sort-by-bday (database)
+  (dump-db (sort database 'record-by-birthday-p)))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+		       :direction :output
+		       :if-exists :supersede)
+    (with-standard-io-syntax (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax (setf *db* (read in)))))
+
+(defun clear-db ()
+  (setq *db* nil))
